6623b44e08e807ffa48b10b2056db08e9d85e2ff
unknown <ahmed.anwar@bixls.com>
2015-12-21 19:30:22 +0200

    welcome screen

--- index.php
+++ index.php
@@ -4,8 +4,19 @@ if(isset($_SESSION["username"])||isset($_COOKIE["username"])){
   if(isset($_COOKIE["username"])){
     $_SESSION['username']=$_COOKIE["username"];
   }
-//Index
-echo("index");
+
+$username=$_SESSION['username'];
+$type=0;
+  require_once("config.php");
+   $dbConnect=new DatabaseConnect;
+
+   $query = mysql_query("SELECT * FROM `user`  WHERE `email` ='$username'") or die (mysql_error());
+   if ($query){
+       $row = mysql_fetch_array($query,MYSQL_ASSOC);
+echo($row["name"]);
+//row array now have all the details of the user
+     }
+
 }else{
 include ("login.php");
 }
