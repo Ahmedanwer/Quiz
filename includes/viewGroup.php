c5065b12a1b0aa601110bb8b044be0d0a1a3f081
ebadawy <ehalbadawy93@gmail.com>
2015-12-22 16:17:50 +0200

    Show group's quizzes

--- includes/viewGroup.php
+++ includes/viewGroup.php
@@ -19,12 +19,47 @@
     <h1><?php echo($group['name']); ?></h1>
     <?php if ($is_joined) {
       echo('<button type="button" class="btn btn-danger" onClick="group_action(\'leave\', \''.$_GET['group_id'].'\', \''.$currentUser['ID'].'\')">Leave</button>');
+      $show_quizes_query = mysql_query("SELECT * FROM `quiz` WHERE quiz.group_id=".$_GET['group_id']) or die (mysql_error());
+
     } else {
       echo('<button type="button" class="btn btn-danger" onClick="group_action(\'join\', \''.$_GET['group_id'].'\', \''.$currentUser['ID'].'\')">Join</button>');
     }
     ?>
     </div>
 </div>
+
+<?php
+  if($is_joined) {
+?>
+<table class="table table-striped">
+     <thead>
+       <tr>
+         <th>ID</th>
+         <th>Title</th>
+         <th>Description</th>
+         <th>Toal Marks</th>
+       </tr>
+     </thead>
+     <tbody>
+      <?php
+      while ($row = mysql_fetch_array($show_quizes_query, MYSQL_ASSOC)) {
+?>
+
+       <tr>
+         <td><?php  echo($row['ID']); ?></td>
+         <td><?php  echo($row['title']); ?></td>
+          <td><?php  echo($row['description']); ?></td>
+          <td><?php  echo($row['total_marks']); ?></td>
+       </tr>
+  <?php
+}
+?>
+
+     </tbody>
+   </table>
+<?php
+  }
+?>
 
 
 <script type="text/javascript">
