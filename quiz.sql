02f23bff4fabbd4971d55d37d7b1cbe6c5039615
Alaa ElNouby <alaaelnouby@gmail.com>
2015-12-26 00:54:14 +0200

    cleaning and fixing quiz.sql

--- quiz.sql
+++ quiz.sql
@@ -1,149 +1,73 @@
--- phpMyAdmin SQL Dump
--- version 4.0.4.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Dec 21, 2015 at 05:08 PM
--- Server version: 5.5.32
--- PHP Version: 5.4.16
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `quiz`
---
-CREATE DATABASE IF NOT EXISTS `quiz` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `quiz`;
 USE `quiz`;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `answer`
---
+CREATE TABLE IF NOT EXISTS `user` (
+  `ID` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL UNIQUE,
+  `faculty` varchar(200) NOT NULL,
+  `university` varchar(200) NOT NULL,
+  `department` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL ,
+  `type` int(11) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `question` (
+  `ID` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `question` varchar(250) NOT NULL,
+  `right_answer` int(11) NOT NULL,
+  `quiz_ID` int(11) NOT NULL REFERENCES quiz(ID)
+);
+
+CREATE TABLE IF NOT EXISTS `choices` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `question_id` int(11) NOT NULL REFERENCES question(ID) ,
+  `choice` varchar(250) NOT NULL,
+  PRIMARY KEY (`ID`,`question_id`)
+);
 
 CREATE TABLE IF NOT EXISTS `answer` (
   `user_id` int(11) NOT NULL,
   `question_id` int(11) NOT NULL,
-  `student_answer` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `student_answer` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`, `question_id`)
+);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `choices`
---
-
-CREATE TABLE IF NOT EXISTS `choices` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `question_id` int(11) NOT NULL,
-  `choice` varchar(250) NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `question_id` (`question_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `group`
---
 
 CREATE TABLE IF NOT EXISTS `group` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID` int(11) PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(250) NOT NULL,
   `subject` varchar(250) NOT NULL,
   `year` varchar(250) NOT NULL,
-  `instructor_id` int(11) NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `instructor_id` (`instructor_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `join`
---
+  `instructor_id` int(11) NOT NULL REFERENCES user(ID)
+);
 
 CREATE TABLE IF NOT EXISTS `join` (
-  `group_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL REFERENCES group(ID),
   `student_id` int(11) NOT NULL,
-  KEY `group_id` (`group_id`),
-  KEY `student_id` (`student_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `question`
---
-
-CREATE TABLE IF NOT EXISTS `question` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `question` varchar(250) NOT NULL,
-  `right_answer` int(11) NOT NULL,
-  `quiz_ID` int(11) NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `quiz_ID` (`quiz_ID`),
-  KEY `quiz_ID_2` (`quiz_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `quiz`
---
+  PRIMARY KEY (`group_id`,`student_id`)
+);
 
 CREATE TABLE IF NOT EXISTS `quiz` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID` int(11) PRIMARY KEY AUTO_INCREMENT,
   `title` varchar(250) NOT NULL,
   `description` varchar(250) NOT NULL,
   `total_marks` int(11) NOT NULL,
-  `instructor_id` int(11) NOT NULL,
-  `group_id` int(11) NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `group_id` (`group_id`),
-  KEY `instructor_id` (`instructor_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `take`
---
+  `instructor_id` int(11) NOT NULL REFERENCES user(ID),
+  `group_id` int(11) NOT NULL REFERENCES group(ID)
+);
 
 CREATE TABLE IF NOT EXISTS `take` (
-  `quiz_id` int(11) NOT NULL,
-  `student_id` int(11) NOT NULL,
+  `quiz_id` int(11) NOT NULL REFERENCES quiz(ID),
+  `student_id` int(11) NOT NULL REFERENCES user(ID),
   `student_mark` int(11) NOT NULL,
-  KEY `student_id` (`student_id`),
-  KEY `quiz_id` (`quiz_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY ('quiz_id','student_id')
+);
 
--- --------------------------------------------------------
 
---
--- Table structure for table `user`
---
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `faculty` varchar(200) NOT NULL,
-  `university` varchar(200) NOT NULL,
-  `department` varchar(200) NOT NULL,
-  `password` varchar(200) NOT NULL,
-  `type` int(11) NOT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
